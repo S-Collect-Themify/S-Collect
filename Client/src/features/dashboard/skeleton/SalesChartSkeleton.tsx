7238d3fc59340514b6f0041c1b6f80927944159f
Ali Waleed <aw3043622@gmail.com>
2026-08-02 11:07:24 +0300

    edit

--- Client/src/features/dashboard/skeleton/SalesChartSkeleton.tsx
+++ Client/src/features/dashboard/skeleton/SalesChartSkeleton.tsx
@@ -7,39 +7,41 @@ const SalesChartSkeleton = () => {
       </div>
 
       {/* Chart Area */}
-      <div className="h-[450px] relative">
+      <div className="h-[400px] relative">
         {/* Y-axis labels */}
-        <div className="absolute left-0 top-0 flex h-full flex-col justify-between">
-          {Array.from({ length: 6 }).map((_, i) => (
+        <div className="absolute left-0 top-0 bottom-8 flex flex-col justify-between">
+          {Array.from({ length: 5 }).map((_, i) => (
             <div key={i} className="h-3 w-8 bg-gray-200 rounded" />
           ))}
         </div>
 
         {/* Grid lines */}
-        <div className="ml-12 h-full flex flex-col justify-between">
-          {Array.from({ length: 6 }).map((_, i) => (
+        <div className="ml-12 h-[360px] flex flex-col justify-between">
+          {Array.from({ length: 5 }).map((_, i) => (
             <div key={i} className="border-t border-gray-100 w-full" />
           ))}
         </div>
 
         {/* Fake line chart */}
-        <svg
-          className="absolute inset-0 ml-12 mt-4"
-          viewBox="0 0 500 250"
-          preserveAspectRatio="none"
-        >
-          <polyline
-            points="20,180 100,120 180,150 260,70 340,100 420,30"
-            fill="none"
-            stroke="#e5e7eb"
-            strokeWidth="6"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          />
-        </svg>
+        <div className="absolute top-0 bottom-10 left-12 right-0">
+          <svg
+            className="w-full h-full"
+            viewBox="0 0 500 200"
+            preserveAspectRatio="none"
+          >
+            <polyline
+              points="20,160 100,100 180,130 260,50 340,80 460,20"
+              fill="none"
+              stroke="#e5e7eb"
+              strokeWidth="5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </div>
 
         {/* X-axis labels */}
-        <div className="absolute bottom-0 left-12 right-0 flex justify-between">
+        <div className="absolute bottom-0 left-12 right-0 flex justify-between pt-2">
           {Array.from({ length: 6 }).map((_, i) => (
             <div key={i} className="h-3 w-12 bg-gray-200 rounded" />
           ))}
