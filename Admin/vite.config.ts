b8a9571e91bb31ae371c9b38e3d9f1299ae2fb53
ahmedwael29 <wael92318@gmail.com>
2026-09-14 10:59:04 +0300

    chore: update codebase

--- Admin/vite.config.ts
+++ Admin/vite.config.ts
@@ -6,6 +6,9 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react(), tailwindcss()],
+    build: {
+      chunkSizeWarningLimit: 1000,
+    },
     server: {
       proxy: {
         '/api/v1': {
